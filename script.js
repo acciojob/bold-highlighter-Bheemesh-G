3d2a9dfd3e2281b00a8f1e142e9f992e50851e6a
Bheemesh Gadige <gbheemesh888@gmail.com>
2024-10-09 18:44:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,21 @@ function highlight() {
     //Write your code here
 
 
+	const arr = document.querySelectorAll('strong');
+	arr.forEach(strong =>{
+		strong.style.color = "green";
+	});
+
+	
 }
 
 
 function return_normal() {
     //Write your code here
 
+	const arr = document.querySelectorAll('strong');
+	arr.forEach(strong=>{
+		strong.style.color = "black";
+	});
     
 }
